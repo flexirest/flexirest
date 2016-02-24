ab283d5ca23fe49a617a82bac390abcae5ed1949
jgu <jgu@jeffreygu.com>
2016-02-24 16:19:18 -0700

    add data to body of connection delete

--- lib/flexirest/connection.rb
+++ lib/flexirest/connection.rb
@@ -69,12 +69,13 @@ module Flexirest
       end
     end
 
-    def delete(path, options={})
+    def delete(path, data, options={})
       set_defaults(options)
       make_safe_request(path) do
         @session.delete(path) do |req|
           set_per_request_timeout(req, options) if options[:timeout]
           req.headers = req.headers.merge(options[:headers])
+          req.body = data
           sign_request(req, options[:api_auth])
         end
       end
